733f738aa79bdf02afe1dfaadcae5fe28f2588fb
Ray Ozzie <ray@ozzie.net>
2022-03-02 11:24:05 -0500

    m

--- go.mod
+++ go.mod
@@ -14,12 +14,13 @@ require (
 	github.com/go-ole/go-ole v1.2.4 // indirect
 	github.com/gofrs/flock v0.7.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/jessevdk/go-flags v1.5.0 // indirect
 	github.com/sendgrid/rest v2.6.8+incompatible // indirect
 	github.com/shirou/gopsutil/v3 v3.21.6 // indirect
 	github.com/slack-go/slack v0.10.2 // indirect
 	github.com/tklauser/go-sysconf v0.3.6 // indirect
 	github.com/tklauser/numcpus v0.2.2 // indirect
 	go.bug.st/serial v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20210316164454-77fc1eacc6aa // indirect
+	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
 	periph.io/x/periph v3.6.2+incompatible // indirect
 )
--- slack.go
+++ slack.go
@@ -8,8 +8,22 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/jessevdk/go-flags"
 	"github.com/slack-go/slack"
 )
+
+// Request parameters
+type readParameters struct {
+}
+
+type writeParameters struct {
+}
+
+type watcherArgs struct {
+	Verbose bool           `short:"v" long:"verbose" description:"Display logging"`
+	Read    readParameters `command:"read" alias:"r" description:"Read functions"`
+	Write   readParameters `command:"write" alias:"w" description:"Write functions"`
+}
 
 // Send a message to Slack.  See:
 // https://api.slack.com/reference/messaging/payload
@@ -46,6 +60,13 @@ func inboundSlackRequestHandler(w http.ResponseWriter, r *http.Request) {
 
 // Slack /watcher request handler
 func slackCommandWatcher(s slack.SlashCommand) (response string) {
-	response = fmt.Sprintf("echo: %v", s.Text)
-	return
+
+	p := flags.NewParser(new(watcherArgs), flags.Default)
+	_, err := p.Parse()
+	if err != nil {
+		return fmt.Sprintf("%s", err)
+	}
+
+	return fmt.Sprintf("%v", p)
+
 }
