b557bd8281f901d8c904a1392fd7ff7ec8883ef3
Ray Ozzie <ray@ozzie.net>
2022-03-05 10:15:47 -0500

    m

--- slack.go
+++ slack.go
@@ -66,7 +66,7 @@ func inboundSlackRequestHandler(w http.ResponseWriter, r *http.Request) {
 			},
 		}
 		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Content-type", "application/json")
 		slackResponse := slack.WebhookMessage{}
 		slackResponse.Blocks = &blocks
 		slackResponseJSON, _ := json.Marshal(slackResponse)
