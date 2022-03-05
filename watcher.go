7f263cb973434c89b90e4d137047094f10b89c06
Ray Ozzie <ray@ozzie.net>
2022-03-05 10:39:58 -0500

    m

--- watcher.go
+++ watcher.go
@@ -284,6 +284,7 @@ func watcherGetHandlerStats(addr string) (response string, errstr string) {
 		response += indent + indent + "Databases (r+w rMs/wMs)" + eol
 		for k, _ := range stats[0].Databases {
 			response += indent + indent + indent + k + eol
+			response += "`"
 			response += indent + indent + indent + indent
 			for _, stat := range stats {
 				response += fmt.Sprintf("%d+%d\t", stat.Databases[k].Reads, stat.Databases[k].Writes)
@@ -294,6 +295,7 @@ func watcherGetHandlerStats(addr string) (response string, errstr string) {
 				response += fmt.Sprintf("%d/%d\t", stat.Databases[k].ReadMs, stat.Databases[k].WriteMs)
 			}
 			response += eol
+			response += "`"
 		}
 
 		// Cache stats
