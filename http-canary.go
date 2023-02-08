7c817a842bdd2c91ba45f7251e10c99436b4a053
Ray Ozzie <ray@ozzie.net>
2023-02-08 15:45:34 -0500

    m

--- http-canary.go
+++ http-canary.go
@@ -6,13 +6,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
+
+	"github.com/blues/note-go/note"
 )
 
 // Canary handler
 func inboundWebCanaryHandler(httpRsp http.ResponseWriter, httpReq *http.Request) {
+
+	// Exit if someone is probing us
+	if httpReq.Method == "GET" {
+		return
+	}
 
 	// Get the body if supplied
 	eventJSON, err := ioutil.ReadAll(httpReq.Body)
@@ -20,7 +29,33 @@ func inboundWebCanaryHandler(httpRsp http.ResponseWriter, httpReq *http.Request)
 		eventJSON = []byte("{}")
 	}
 
+	// Unmarshal to an event
+	var e note.Event
+	err = json.Unmarshal(eventJSON, &e)
+	if err != nil {
+		return
+	}
+
+	// Ignore non-data events
+	if e.NotefileID != "_temp.qo" {
+		return
+	}
+
+	// Determine the various latencies
+	capturedTime := e.When
+	receivedTime := int64(e.Received)
+	routedTime := e.Routed
+	canaryTime := time.Now().UTC().Unix()
+	body := *e.Body
+	seqNo := body["count"]
+	sessionID := e.SessionUID
+
 	// Determine the difference between note creation time and the time of receipt
-	fmt.Printf("\n%s\n", eventJSON)
+	fmt.Printf("\ncaptured: %d\n", capturedTime)
+	fmt.Printf("\nreceived: %d\n", receivedTime)
+	fmt.Printf("\nrouted: %d\n", routedTime)
+	fmt.Printf("\nnow: %d\n", canaryTime)
+	fmt.Printf("\nseqno: %d\n", seqNo)
+	fmt.Printf("\nsessionID: %s\n", sessionID)
 
 }
