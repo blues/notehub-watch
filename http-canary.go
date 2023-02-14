190472f3622cae8b44397d29b284931d94fb5b55
Ray Ozzie <ray@ozzie.net>
2023-02-13 22:04:48 -0500

    fixes

--- http-canary.go
+++ http-canary.go
@@ -18,28 +18,34 @@ import (
 )
 
 // Retained between canary notifications
+type deviceContext struct {
+	continuous bool
+	warnings   int64
+}
 type lastEvent struct {
-	continuous   bool
 	sessionID    string
 	seqNo        int64
 	capturedTime int64
 	receivedTime int64
 	routedTime   int64
-	warnings     int64
 }
 
-var lastDeviceEventLock sync.Mutex
-var lastDeviceEvent map[string]lastEvent
+var canaryLock sync.Mutex
+var last map[string]lastEvent
+var device map[string]deviceContext
 
 // Canary handler
 func inboundWebCanaryHandler(httpRsp http.ResponseWriter, httpReq *http.Request) {
 
 	// Instantiate the map
-	lastDeviceEventLock.Lock()
-	if lastDeviceEvent == nil {
-		lastDeviceEvent = map[string]lastEvent{}
+	canaryLock.Lock()
+	if last == nil {
+		last = map[string]lastEvent{}
 	}
-	lastDeviceEventLock.Unlock()
+	if device == nil {
+		device = map[string]deviceContext{}
+	}
+	canaryLock.Unlock()
 
 	// Exit if someone is probing us
 	if httpReq.Method == "GET" {
@@ -61,14 +67,14 @@ func inboundWebCanaryHandler(httpRsp http.ResponseWriter, httpReq *http.Request)
 
 	// Remember info about the last session
 	if e.NotefileID == "_session.qo" {
-		lastDeviceEventLock.Lock()
-		last, present := lastDeviceEvent[e.DeviceUID]
+		canaryLock.Lock()
+		d, present := device[e.DeviceUID]
 		if present && e.Body != nil {
 			body := *e.Body
-			last.continuous = strings.Contains(body["why"].(string), "continuous")
+			d.continuous = strings.Contains(body["why"].(string), "continuous")
 		}
-		lastDeviceEvent[e.DeviceUID] = last
-		lastDeviceEventLock.Unlock()
+		device[e.DeviceUID] = d
+		canaryLock.Unlock()
 		return
 	}
 
@@ -78,35 +84,36 @@ func inboundWebCanaryHandler(httpRsp http.ResponseWriter, httpReq *http.Request)
 	}
 
 	// Determine the various latencies
-	var this lastEvent
-	this.sessionID = e.SessionUID
-	this.capturedTime = e.When
-	this.receivedTime = int64(e.Received)
-	this.routedTime = time.Now().UTC().Unix()
+	var t lastEvent
+	t.sessionID = e.SessionUID
+	t.capturedTime = e.When
+	t.receivedTime = int64(e.Received)
+	t.routedTime = time.Now().UTC().Unix()
 	if e.Body != nil {
 		body := *e.Body
-		this.seqNo = int64(body["count"].(float64))
+		t.seqNo = int64(body["count"].(float64))
 	}
 
 	// Alert
-	lastDeviceEventLock.Lock()
+	canaryLock.Lock()
 	errstr := ""
-	last, present := lastDeviceEvent[e.DeviceUID]
+	d, present := device[e.DeviceUID]
 	if present {
-		if this.continuous && this.sessionID != last.sessionID {
-			errstr = "continuous session dropped and reconnected: " + this.sessionID
-		} else if this.seqNo != last.seqNo+1 {
-			errstr = fmt.Sprintf("sequence out of order (expected %d but received %d): %s", last.seqNo+1, this.seqNo, e.EventUID)
-		} else if (this.receivedTime - this.capturedTime) > 120 {
-			errstr = fmt.Sprintf("event took %d secs to get from notecard to notehub: %s", this.receivedTime-this.capturedTime, e.EventUID)
-		} else if (this.routedTime - this.receivedTime) > 10 {
-			errstr = fmt.Sprintf("event took %d secs to be routed once it was received by notehub: %s", this.routedTime-this.receivedTime, e.EventUID)
-		} else if (this.receivedTime - last.receivedTime) > 5*60 {
-			errstr = fmt.Sprintf("%d minutes between events received by notehub: %s", (this.routedTime-this.receivedTime)/60, e.EventUID)
+		l := last[e.DeviceUID]
+		if d.continuous && t.sessionID != l.sessionID {
+			errstr = "continuous session dropped and reconnected: " + t.sessionID
+		} else if t.seqNo != l.seqNo+1 {
+			errstr = fmt.Sprintf("sequence out of order (expected %d but received %d): %s", l.seqNo+1, t.seqNo, e.EventUID)
+		} else if (t.receivedTime - t.capturedTime) > 120 {
+			errstr = fmt.Sprintf("event took %d secs to get from notecard to notehub: %s", t.receivedTime-t.capturedTime, e.EventUID)
+		} else if (t.routedTime - t.receivedTime) > 10 {
+			errstr = fmt.Sprintf("event took %d secs to be routed once it was received by notehub: %s", t.routedTime-t.receivedTime, e.EventUID)
+		} else if (t.receivedTime - l.receivedTime) > 5*60 {
+			errstr = fmt.Sprintf("%d minutes between events received by notehub: %s", (t.routedTime-t.receivedTime)/60, e.EventUID)
 		}
 	}
-	lastDeviceEvent[e.DeviceUID] = this
-	lastDeviceEventLock.Unlock()
+	last[e.DeviceUID] = t
+	canaryLock.Unlock()
 
 	// Send message
 	if errstr != "" {
@@ -119,29 +126,33 @@ func inboundWebCanaryHandler(httpRsp http.ResponseWriter, httpReq *http.Request)
 func canarySweepDevices() {
 
 	// Instantiate the map
-	lastDeviceEventLock.Lock()
-	if lastDeviceEvent == nil {
-		lastDeviceEvent = map[string]lastEvent{}
+	canaryLock.Lock()
+	if last == nil {
+		last = map[string]lastEvent{}
 	}
-	lastDeviceEventLock.Unlock()
+	if device == nil {
+		device = map[string]deviceContext{}
+	}
+	canaryLock.Unlock()
 
 	// Look at the map to see if there's anything due
 
-	lastDeviceEventLock.Lock()
+	canaryLock.Lock()
 	now := time.Now().UTC().Unix()
-	for deviceUID, last := range lastDeviceEvent {
-		if now-last.receivedTime >= 6*60 {
-			last.warnings++
-			lastDeviceEvent[deviceUID] = last
-			if last.warnings < 10 {
-				canaryMessage(deviceUID, fmt.Sprintf("no routed events received in %d minutes (last event received %s)", (now-last.receivedTime)/60,
-					time.Unix(last.receivedTime, 0).UTC().Format("01-02 15:04:05")))
-			} else if last.warnings == 10 {
+	for deviceUID, d := range device {
+		l := last[deviceUID]
+		if now-l.receivedTime >= 6*60 {
+			d.warnings++
+			device[deviceUID] = d
+			if d.warnings < 10 {
+				canaryMessage(deviceUID, fmt.Sprintf("no routed events received in %d minutes (last event received %s)", (now-l.receivedTime)/60,
+					time.Unix(l.receivedTime, 0).UTC().Format("01-02 15:04:05")))
+			} else if d.warnings == 10 {
 				canaryMessage(deviceUID, "LAST WARNING before silence!")
 			}
 		}
 	}
-	lastDeviceEventLock.Unlock()
+	canaryLock.Unlock()
 
 }
 
